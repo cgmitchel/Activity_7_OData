b310b08312ff99827a53809de5b13d3326a0cbc1
Mitchel Gaabucayan <cgmitchel@gmail.com>
2025-11-12 23:02:15 +0000

    commit update code

--- odata/webapp/controller/Main.controller.js
+++ odata/webapp/controller/Main.controller.js
@@ -4,7 +4,8 @@ sap.ui.define([
     "use strict";
 
     return Controller.extend("sapips.training.odata.controller.Main", {
-        onInit() {
+        formatPrice: function (price) {
+            return "₱" + Number(price).toFixed(2);
         }
     });
 });
